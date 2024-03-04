9df0462118c6a8b4048785f5fcf6712110805a9b
areejnasirdh <areejdh359gmail.com>
2024-03-04 01:22:50 -0700

    add zego client

--- components/CaseStudy/Zego/OverviewSection.js
+++ components/CaseStudy/Zego/OverviewSection.js
@@ -22,7 +22,7 @@ const Overview = () => {
     // "Gallery Module",
   ];
   return (
-    <OverviewSection logo={booklover} services_list={services_list} CLIENT="ZEGO" industry="AI Automation" >
+    <OverviewSection logo={booklover} services_list={services_list} client="ZEGO" industry="AI Automation" >
       <h5>
         Design Henge collaborates with ZEGO, an AI company, crafting its logo
         and UI to propel automation, AI implementation, and outsourced team
